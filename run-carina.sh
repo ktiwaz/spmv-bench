e8f5204569d9f46234a325ba9f66800badb4a4a6
patrickf2000 <dev@patrickflynn.co>
2024-02-29 10:21:25 -0500

    Initial Serial Carina results

--- run-carina.sh
+++ run-carina.sh
@@ -2,9 +2,9 @@
 
 bin/run_serial.sh "carina"
 bin/run_omp.sh "carina"
-bin/run_omp_collapse.sh "carina"
+#bin/run_omp_collapse.sh "carina"
 bin/run_transpose_serial.sh "carinia"
 bin/run_transpose_omp.sh "carina"
-bin/run_transpose_omp_collapse.sh "carina"
+#bin/run_transpose_omp_collapse.sh "carina"
 bin/run_transpose_omp_omp.sh "carina"
 
