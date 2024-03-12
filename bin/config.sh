ed5dbefd8d9f854eda4ba8839377444f5436a056
Patrick Flynn <pflynn157@gmail.com>
2024-03-12 12:00:03 -0400

    New scripts for specific format tests

--- bin/config.sh
+++ bin/config.sh
@@ -2,14 +2,14 @@ BIN=build/bin
 OLEVELS=("O1" "O2" "O3")
 
 # For benchmarking
-iters=100
-threads=(8 16 32 64)
-blocks=(1 2 4 16)
-k_loop=(16)
+#iters=100
+#threads=(8 16 32 64)
+#blocks=(1 2 4 16)
+#k_loop=(16)
 
 # For testing
-#iters=1
-#threads=(8 16)
-#blocks=(1 4)
-#k_loop=(16)
+iters=1
+threads=(8 16)
+blocks=(1 4)
+k_loop=(16)
 
---
+++ bin/run_bell.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+BENCH_NAME="bell"
+ARCH_ID=$1
+
+source bin/config.sh
+source bin/lib.sh
+setup
+
+##
+## Our run function
+##
+function run() {
+    NAME=$1
+    CSV_FILE=report/csv/$1_"$BENCH_NAME"_"$ARCH_ID".csv
+    init_csv $CSV_FILE
+    
+    for k in "${k_loop[@]}"
+    do
+        for O in "${OLEVELS[@]}"
+        do
+            for b in "${blocks[@]}"
+            do
+                for t in "${threads[@]}"
+                do
+                    echo "[OMP] bell --k $k ${b} --threads $t"
+                    printf "bell_omp_${O}," >> $CSV_FILE
+                    $BIN/bell_omp_${O} data/$NAME.mtx --iters $iters --k $k --block $b --threads $t >> $CSV_FILE
+                    
+                    echo "[OMP Collapse] bell --k $k ${b} --threads $t"
+                    printf "bell_omp_collapse_${O}," >> $CSV_FILE
+                    $BIN/bell_omp_collapse_${O} data/$NAME.mtx --iters $iters --k $k --block $b --threads $t >> $CSV_FILE
+                    
+                    echo "[Transpose OMP] bell --k $k ${b} --threads $t"
+                    printf "bell_transpose_omp_${O}," >> $CSV_FILE
+                    $BIN/bell_transpose_omp_${O} data/$NAME.mtx --iters $iters --k $k --block $b --threads $t >> $CSV_FILE
+                    
+                    echo "[Transpose OMP Collapse] bell --k $k ${b} --threads $t"
+                    printf "bell_transpose_omp_collapse_${O}," >> $CSV_FILE
+                    $BIN/bell_transpose_omp_collapse_${O} data/$NAME.mtx --iters $iters --k $k --block $b --threads $t >> $CSV_FILE
+                    
+                    echo "[Transpose OMPxOMP] bell --k $k ${b} --threads $t"
+                    printf "bell_transpose_omp_omp_${O}," >> $CSV_FILE
+                    $BIN/bell_transpose_omp_omp_${O} data/$NAME.mtx --iters $iters --k $k --block $b --threads $t >> $CSV_FILE
+                done
+            done
+        done
+    done
+}
+
+##
+## Run the benchmark on all available data
+##
+for mtx in data/*.mtx
+do
+    input_mtx=`basename $mtx .mtx`
+    run $input_mtx
+done
+
