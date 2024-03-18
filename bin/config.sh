1c648eb7c50c20a11d404ad2b4f7981d4c85c7b2
Patrick Flynn <pflynn157@gmail.com>
2024-03-18 13:41:49 -0400

    Changes to run config; new run scripts

--- bin/config.sh
+++ bin/config.sh
@@ -1,13 +1,22 @@
 BIN=build/bin
-OLEVELS=("O1" "O2" "O3")
 
-# For benchmarking
-iters=100
-threads=(8 16 32 64)
+
+# For benchmarking (Old)
+#OLEVELS=("O1" "O2" "O3")
+#iters=100
+#threads=(8 16 32 64)
+#blocks=(1 2 4 16)
+#k_loop=(16)
+
+# For benchmarking (Shorter- hopefully)
+OLEVELS=("O2")
+iters=10
+threads=(8 16 32)
 blocks=(1 2 4 16)
 k_loop=(16)
 
 # For testing
+#OLEVELS=("O1" "O2" "O3")
 #iters=1
 #threads=(8 16)
 #blocks=(1 4)
