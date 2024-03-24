ef521eb32fc22169ceb09336766a4ac62a3cb21e
patrickf2000 <dev@patrickflynn.co>
2024-03-24 11:38:50 -0400

    Benchmark data on Carina (RMA10/COP20k_A)

--- run-carina.sh
+++ run-carina.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
-#echo ""
-#echo "--------------------"
-#echo "Running COO"
-#echo "--------------------"
-#bin/run_coo.sh "carina_O3"
+echo ""
+echo "--------------------"
+echo "Running COO"
+echo "--------------------"
+bin/run_coo.sh "carina"
 
 echo ""
 echo "--------------------"
 echo "Running CSR"
 echo "--------------------"
-bin/run_csr.sh "carina_O3"
+bin/run_csr.sh "carina"
 
 echo ""
 echo "--------------------"
 echo "Running ELL"
 echo "--------------------"
-bin/run_ell.sh "carina_O3"
+bin/run_ell.sh "carina"
 
 echo ""
 echo "--------------------"
 echo "Running BCSR"
 echo "--------------------"
-bin/run_bcsr.sh "carina_O3"
+bin/run_bcsr.sh "carina"
 
 echo ""
 echo "--------------------"
 echo "Running BELL"
 echo "--------------------"
-bin/run_bell.sh "carina_O3"
+bin/run_bell.sh "carina"
 
 echo ""
 echo "--------------------"
 echo "Running serial"
 echo "--------------------"
-bin/run_serial.sh "carina_O3"
+bin/run_serial.sh "carina"
 
