0a4e97bb69bac3cc55ee532e564fa98e91b16c2d
Patrick Flynn <pflynn157@gmail.com>
2024-04-03 11:23:56 -0400

    New intel script

--- run-intel.sh
+++ run-intel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-bin/run_coo.sh "intel"
-bin/run_csr.sh "intel"
+#bin/run_coo.sh "intel"
+#bin/run_csr.sh "intel"
 #bin/run_ell.sh "intel"
 #bin/run_bcsr.sh "intel"
 
@@ -9,4 +9,27 @@ bin/run_csr.sh "intel"
 #bin/run_csr_transpose.sh "intel"
 #bin/run_ell_transpose.sh "intel"
 #bin/run_bcsr_transpose.sh "intel"
+
+bin/coo/serial.sh "intel"
+bin/csr/serial.sh "intel"
+bin/ell/serial.sh "intel"
+bin/bcsr/serial.sh "intel"
+
+bin/coo/omp.sh "intel"
+bin/csr/omp.sh "intel"
+bin/ell/omp.sh "intel"
+bin/bcsr/omp.sh "intel"
+
+bin/coo/serial_transpose.sh "intel"
+bin/csr/serial_transpose.sh "intel"
+bin/ell/serial_transpose.sh "intel"
+bin/bcsr/serial_transpose.sh "intel"
+
+bin/coo/omp_transpose.sh "intel"
+bin/csr/omp_transpose.sh "intel"
+bin/ell/omp_transpose.sh "intel"
+bin/bcsr/omp_transpose.sh "intel"
+
+bin/coo/cusparse.sh "intel"
+bin/csr/cusparse.sh "intel"
 
