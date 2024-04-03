1990de309fae589cb27b4e38dd3bf9b143846736
Patrick Flynn <pflynn157@gmail.com>
2024-04-03 10:05:25 -0400

    Updated Arm script

--- run-arm.sh
+++ run-arm.sh
@@ -2,11 +2,13 @@
 
 #bin/run_coo.sh "arm"
 #bin/run_csr.sh "arm"
-bin/run_ell.sh "arm"
-bin/run_bcsr.sh "arm"
+#bin/run_ell.sh "arm"
+#bin/run_bcsr.sh "arm"
+
+bin/run-cusparse.sh "arm"
 
 #bin/run_coo_transpose.sh "arm"
-#bin/run_csr_transpose.sh "arm"
-#bin/run_ell_transpose.sh "arm"
-#bin/run_bcsr_transpose.sh "arm"
+bin/run_csr_transpose.sh "arm"
+bin/run_ell_transpose.sh "arm"
+bin/run_bcsr_transpose.sh "arm"
 
