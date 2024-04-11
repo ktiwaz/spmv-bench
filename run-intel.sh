31de43c3e2004cb3a20a0a4a0c9d5b1fa332c758
Patrick Flynn <pflynn157@gmail.com>
2024-04-11 09:37:19 -0400

    OMP Transpose data for Intel

--- run-intel.sh
+++ run-intel.sh
@@ -8,17 +8,17 @@
 #bin/run_coo_transpose.sh "intel"
 #bin/run_csr_transpose.sh "intel"
 #bin/run_ell_transpose.sh "intel"
-bin/run_bcsr_transpose.sh "intel"
+#bin/run_bcsr_transpose.sh "intel"
 
 #bin/coo/omp.sh "intel"
 #bin/csr/omp.sh "intel"
 #bin/ell/omp.sh "intel"
 #bin/bcsr/omp.sh "intel"
 
-bin/coo/omp_threads.sh "intel"
-bin/csr/omp_threads.sh "intel"
-bin/ell/omp_threads.sh "intel"
-bin/bcsr/omp_threads.sh "intel"
+#bin/coo/omp_threads.sh "intel"
+#bin/csr/omp_threads.sh "intel"
+#bin/ell/omp_threads.sh "intel"
+#bin/bcsr/omp_threads.sh "intel"
 
 bin/coo/omp_transpose.sh "intel"
 bin/csr/omp_transpose.sh "intel"
