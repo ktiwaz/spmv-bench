cc1c78260bd632a0b8002bbf42f6167a7d4842f1
Patrick Flynn <pflynn157@gmail.com>
2024-04-19 09:00:02 -0400

    New Intel transpsoe data

--- run-intel.sh
+++ run-intel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-bin/coo/run_coo_transpose.sh "intel"
-bin/csr/run_csr_transpose.sh "intel"
-bin/ell/run_ell_transpose.sh "intel"
-bin/bcsr/run_bcsr_transpose.sh "intel"
+bin/run_coo_transpose.sh "intel"
+bin/run_csr_transpose.sh "intel"
+bin/run_ell_transpose.sh "intel"
+bin/run_bcsr_transpose.sh "intel"
 
