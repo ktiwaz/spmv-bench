3fac3b53abc256b43048c6e3ef1e9ff19dd5ce07
Patrick Flynn <pflynn157@gmail.com>
2024-04-23 09:55:44 -0400

    intel k data

--- run-intel.sh
+++ run-intel.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-bin/run_coo_transpose.sh "intel"
-bin/run_csr_transpose.sh "intel"
-bin/run_ell_transpose.sh "intel"
-bin/run_bcsr_transpose.sh "intel"
+bin/coo/omp.sh "intel_k"
+bin/csr/omp.sh "intel_k"
+bin/ell/omp.sh "intel_k"
+bin/bcsr/omp.sh "intel_k"
+
+#bin/run_coo_transpose.sh "intel"
+#bin/run_csr_transpose.sh "intel"
+#bin/run_ell_transpose.sh "intel"
+#bin/run_bcsr_transpose.sh "intel"
 
