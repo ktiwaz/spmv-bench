d8efa469e45da90bbbf31e2092e60b1d7383cf91
Kushagra Tiwari <ktiwari@ucdavis.edu>
2025-02-21 20:20:16 -0800

    config.mk: assume clang/clang++ is in user's path

--- config.mk
+++ config.mk
@@ -1,8 +1,8 @@
-#CC=clang
-#CXX=clang++
+CC=clang
+CXX=clang++
 
-CC=/data/opt/llvm/bin/clang
-CXX=/data/opt/llvm/bin/clang++
+# CC=/data/opt/llvm/bin/clang
+# CXX=/data/opt/llvm/bin/clang++
 
 CXXFLAGS=-Isrc build/libspmm.a -std=c++17 -march=native -fopenmp
 #OMPFLAGS=-fopenmp
