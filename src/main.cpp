16605805d96b5fb7266de73d8683f043889ccb7a
Kushagra Tiwari <ktiwari@ucdavis.edu>
2025-03-07 12:43:56 -0800

    add perf profiling

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "matrix.h"
-#define _GNU_SOURCE
 #include <linux/perf_event.h>
 #include <sys/ioctl.h>
 #include <sys/syscall.h>
@@ -17,24 +16,6 @@ static long perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
     return syscall(SYS_perf_event_open, hw_event, pid, cpu, group_fd, flags);
 }
 
-// Helper function to initialize a perf event
-int setup_perf_event(struct perf_event_attr *pe, int config, int group_fd) {
-    memset(pe, 0, sizeof(struct perf_event_attr));
-    pe->type = PERF_TYPE_HARDWARE;
-    pe->size = sizeof(struct perf_event_attr);
-    pe->config = config;
-    pe->disabled = 1;
-    pe->exclude_kernel = 1;
-    pe->exclude_hv = 1;
-
-    int fd = perf_event_open(pe, 0, -1, group_fd, 0);
-    if (fd == -1) {
-        fprintf(stderr, "Error opening perf event: %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-    return fd;
-}
-
 // Function to get time in seconds
 double get_time() {
     struct timeval tv;
@@ -45,71 +26,111 @@ double get_time() {
 int main(int argc, char **argv) {
     Matrix mtx(argc, argv);
     mtx.format();
-
-    struct perf_event_attr pe;
     
-    // Open multiple events
-    int cycles_fd = setup_perf_event(&pe, PERF_COUNT_HW_CPU_CYCLES, -1);
-    int instructions_fd = setup_perf_event(&pe, PERF_COUNT_HW_INSTRUCTIONS, cycles_fd);
-    int branches_fd = setup_perf_event(&pe, PERF_COUNT_HW_BRANCH_INSTRUCTIONS, cycles_fd);
-    int branch_misses_fd = setup_perf_event(&pe, PERF_COUNT_HW_BRANCH_MISSES, cycles_fd);
 
-    // Get initial system resource usage
-    struct rusage usage_start, usage_end;
-    getrusage(RUSAGE_SELF, &usage_start);
+    struct perf_event_attr pe_cycles, pe_instructions, pe_cache_refs, pe_cache_misses, pe_branches, pe_branch_misses;
+    memset(&pe_cycles, 0, sizeof(struct perf_event_attr));
+    memset(&pe_instructions, 0, sizeof(struct perf_event_attr));
+    memset(&pe_cache_refs, 0, sizeof(struct perf_event_attr));
+    memset(&pe_cache_misses, 0, sizeof(struct perf_event_attr));
+    memset(&pe_branches, 0, sizeof(struct perf_event_attr));
+    memset(&pe_branch_misses, 0, sizeof(struct perf_event_attr));
 
-    // Start measuring time
-    double start_time = get_time();
-    
-    // Enable counters
-    ioctl(cycles_fd, PERF_EVENT_IOC_RESET, 0);
-    ioctl(cycles_fd, PERF_EVENT_IOC_ENABLE, 0);
+    // Common settings
+    struct perf_event_attr base_pe = {
+        .type = PERF_TYPE_HARDWARE,
+        .size = sizeof(struct perf_event_attr),
+        .disabled = 1,
+        .exclude_kernel = 0,
+        .exclude_hv = 1
+    };
 
+    // Configure events
+    pe_cycles = base_pe;
+    pe_cycles.config = PERF_COUNT_HW_CPU_CYCLES;
+
+    pe_instructions = base_pe;
+    pe_instructions.config = PERF_COUNT_HW_INSTRUCTIONS;
+
+    pe_cache_refs = base_pe;
+    pe_cache_refs.config = PERF_COUNT_HW_CACHE_REFERENCES;
+
+    pe_cache_misses = base_pe;
+    pe_cache_misses.config = PERF_COUNT_HW_CACHE_MISSES;
+
+    pe_branches = base_pe;
+    pe_branches.config = PERF_COUNT_HW_BRANCH_INSTRUCTIONS;
+
+    pe_branch_misses = base_pe;
+    pe_branch_misses.config = PERF_COUNT_HW_BRANCH_MISSES;
+
+    // Open event file descriptors
+    int fd_cycles = perf_event_open(&pe_cycles, 0, -1, -1, 0);
+    int fd_instructions = perf_event_open(&pe_instructions, 0, -1, -1, 0);
+    int fd_cache_refs = perf_event_open(&pe_cache_refs, 0, -1, -1, 0);
+    int fd_cache_misses = perf_event_open(&pe_cache_misses, 0, -1, -1, 0);
+    int fd_branches = perf_event_open(&pe_branches, 0, -1, -1, 0);
+    int fd_branch_misses = perf_event_open(&pe_branch_misses, 0, -1, -1, 0);
+
+    if (fd_cycles == -1 || fd_instructions == -1 || fd_cache_refs == -1 || fd_cache_misses == -1 || 
+        fd_branches == -1 || fd_branch_misses == -1) {
+        perror("perf_event_open");
+        return 1;
+    }
+
+    // Reset and enable counters
+    ioctl(fd_cycles, PERF_EVENT_IOC_RESET, 0);
+    ioctl(fd_instructions, PERF_EVENT_IOC_RESET, 0);
+    ioctl(fd_cache_refs, PERF_EVENT_IOC_RESET, 0);
+    ioctl(fd_cache_misses, PERF_EVENT_IOC_RESET, 0);
+    ioctl(fd_branches, PERF_EVENT_IOC_RESET, 0);
+    ioctl(fd_branch_misses, PERF_EVENT_IOC_RESET, 0);
+
+    ioctl(fd_cycles, PERF_EVENT_IOC_ENABLE, 0);
+    ioctl(fd_instructions, PERF_EVENT_IOC_ENABLE, 0);
+    ioctl(fd_cache_refs, PERF_EVENT_IOC_ENABLE, 0);
+    ioctl(fd_cache_misses, PERF_EVENT_IOC_ENABLE, 0);
+    ioctl(fd_branches, PERF_EVENT_IOC_ENABLE, 0);
+    ioctl(fd_branch_misses, PERF_EVENT_IOC_ENABLE, 0);
 
     mtx.benchmark();
 
     // Disable counters
-    ioctl(cycles_fd, PERF_EVENT_IOC_DISABLE, 0);
+    ioctl(fd_cycles, PERF_EVENT_IOC_DISABLE, 0);
+    ioctl(fd_instructions, PERF_EVENT_IOC_DISABLE, 0);
+    ioctl(fd_cache_refs, PERF_EVENT_IOC_DISABLE, 0);
+    ioctl(fd_cache_misses, PERF_EVENT_IOC_DISABLE, 0);
+    ioctl(fd_branches, PERF_EVENT_IOC_DISABLE, 0);
+    ioctl(fd_branch_misses, PERF_EVENT_IOC_DISABLE, 0);
 
-    // Stop measuring time
-    double end_time = get_time();
-    
-    // Read values
-    long long cycles, instructions, branches, branch_misses;
-    read(cycles_fd, &cycles, sizeof(long long));
-    read(instructions_fd, &instructions, sizeof(long long));
-    read(branches_fd, &branches, sizeof(long long));
-    read(branch_misses_fd, &branch_misses, sizeof(long long));
+    // Read and print results
+    long long cycles, instructions, cache_refs, cache_misses, branches, branch_misses;
+    read(fd_cycles, &cycles, sizeof(long long));
+    read(fd_instructions, &instructions, sizeof(long long));
+    read(fd_cache_refs, &cache_refs, sizeof(long long));
+    read(fd_cache_misses, &cache_misses, sizeof(long long));
+    read(fd_branches, &branches, sizeof(long long));
+    read(fd_branch_misses, &branch_misses, sizeof(long long));
 
-    // Get final system resource usage
-    getrusage(RUSAGE_SELF, &usage_end);
-    
-    double elapsed_time = end_time - start_time;
-    double user_time = (usage_end.ru_utime.tv_sec - usage_start.ru_utime.tv_sec) +
-                       (usage_end.ru_utime.tv_usec - usage_start.ru_utime.tv_usec) / 1e6;
-    double sys_time = (usage_end.ru_stime.tv_sec - usage_start.ru_stime.tv_sec) +
-                      (usage_end.ru_stime.tv_usec - usage_start.ru_stime.tv_usec) / 1e6;
+    printf("CPU Cycles:        %lld\n", cycles);
+    printf("Instructions:      %lld\n", instructions);
+    printf("Cache References:  %lld\n", cache_refs);
+    printf("Cache Misses:      %lld (%.2f%% miss rate)\n", cache_misses,
+           cache_refs ? (100.0 * cache_misses / cache_refs) : 0);
+    printf("Branch Instructions: %lld\n", branches);
+    printf("Branch Misses:       %lld (%.2f%% miss rate)\n", branch_misses,
+           branches ? (100.0 * branch_misses / branches) : 0);
 
-    // Print statistics
-    printf("\nPerformance counter stats for './your_program':\n\n");
-    printf("%16.2f msec task-clock                       #    %.3f CPUs utilized\n", elapsed_time * 1000, 1.0);
-    printf("%16lld      cpu-cycles                      #    %.2f GHz\n", cycles, cycles / (elapsed_time * 1e9));
-    printf("%16lld      instructions                     #    %.2f  insn per cycle\n", instructions, (double)instructions / cycles);
-    printf("%16lld      branches                         #    %.2f M/sec\n", branches, branches / (elapsed_time * 1e6));
-    printf("%16lld      branch-misses                    #    %.2f%% of all branches\n", branch_misses, 100.0 * branch_misses / branches);
-    printf("\n%16.9f seconds time elapsed\n", elapsed_time);
-    printf("%16.9f seconds user\n", user_time);
-    printf("%16.9f seconds sys\n", sys_time);
+    // Close file descriptors
+    close(fd_cycles);
+    close(fd_instructions);
+    close(fd_cache_refs);
+    close(fd_cache_misses);
+    close(fd_branches);
+    close(fd_branch_misses);
 
-    // Close FDs
-    close(cycles_fd);
-    close(instructions_fd);
-    close(branches_fd);
-    close(branch_misses_fd);
-    
     mtx.verify();
     mtx.report();
-    
     return 0;
 }
 
